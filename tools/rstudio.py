e8f54076b81206b660f9e826dd3d63fe95413133
jrnold <jeffrey.arnold@gmail.com>
2016-06-27 19:41:46 -0700

    update rstudio
    - output regexes
    - add deprecated functions

--- tools/rstudio.py
+++ tools/rstudio.py
@@ -7,12 +7,14 @@ import sys
 from datetime import date
 
 import jinja2
-_TEMPLATE = """
-var functionList = "{functions}";
+_TEMPLATE = r"""
+var functionList = "\\b({functions})\\b)";
 
-var distributionList = "{distributions}";
+var distributionList = "\\b({distributions})\\b";
 
-var deprecatedFunctionList = "{deprecated_functions}";
+var deprecatedFunctionList = "\\b({deprecated_functions})\\b";
+
+var reservedWords = "\\b({reserved})\\b"
 """
 
 
@@ -21,15 +23,17 @@ def read_json(filename):
     with open(filename, "r") as f:
         data = json.load(f)
     functions = sorted([k for k, v in data['functions'].items()
-                        if (not v['operator'] and not v['deprecated'])])
+                        if not (v['operator'] or v['deprecated'] or v['keyword'])])
     deprecated_functions = sorted([k for k, v in data['functions'].items()
                         if not v['operator'] and v['deprecated']])
     distributions = sorted([v['sampling'] for k, v in data['functions'].items()
                             if v['sampling']])
+    reserved = sorted(list(set(data['reserved']['cpp'] + data['reserved']['stan'])))
     return {
         'functions': '|'.join(functions),
         'distributions': '|'.join(distributions),
-        'deprecated_functions': '|'.join(deprecated_functions)
+        'deprecated_functions': '|'.join(deprecated_functions),
+        'reserved': '|'.join(reserved)
     }
 
 def main():
